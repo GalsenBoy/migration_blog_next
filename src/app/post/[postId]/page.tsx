667cce8a4dc0762ea8ec06742bfbd4de339f2f5e
bilaly <billycsk@gmail.com>
2024-01-27 14:11:06 +0100

    Intégration complète vers next

--- src/app/post/[postId]/page.tsx
+++ src/app/post/[postId]/page.tsx
@@ -1,8 +1,7 @@
 import IUpload from "@/interfaces/IUpload";
-import { API_REMOTE } from "@/route";
-import Image from "next/image";
 import IPost from "@/interfaces/IPost";
 import { getOnePost } from "@/requests/getPost";
+import "./onepost.scss";
 
 export default async function PostId({
   params,
@@ -19,16 +18,9 @@ export default async function PostId({
           post.upload.map((upload: IUpload, index: number) => (
             <img
               key={index}
-              src={`${API_REMOTE}/${upload.originalname}`}
+              src={`/${upload.originalname}`}
               alt={upload.originalname}
             />
-            // <Image
-
-            //   src=
-
-            //   width={100}
-            //   height={100}
-            // />
           ))}
         <div>
           <p>{post?.description}</p>
